66f1312759e9fd106d5c0fc40705d496e973f004
mziru <91102080+mziru@users.noreply.github.com>
2022-12-18 14:07:20 -0800

    added configurations for cloudSQL and GAE

--- config.py
+++ config.py
@@ -10,8 +10,11 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir,
                            options={"swagger_ui": True})
 
-with open("postgres_url.txt", "r") as file:
+with open("local_postgres_uri.txt", "r") as file:
     postgres_uri = file.read()
+
+# with open("postgres_uri.txt", "r") as file:
+#     postgres_uri = file.read()
 
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = postgres_uri
