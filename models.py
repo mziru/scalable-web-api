a7c59c793deec4c21a9b5a7b967e370f3bca8ca4
mziru <91102080+mziru@users.noreply.github.com>
2022-12-19 11:48:01 -0800

    added comments to improve code legibility

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from config import db, ma, app
 from sqlalchemy import event, DDL
 
 
+# object data model for Person table
 class Person(db.Model):
     __tablename__ = "person"
     name = db.Column(db.String(32), primary_key=True)
@@ -10,9 +11,11 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+    # persons and results have a one-to-many relationship
     results = db.relationship('Result', backref='person', lazy=True)
 
 
+# object data model for Result table
 class Result(db.Model):
     __tablename__ = "result"
     name = db.Column(db.String(32), db.ForeignKey('person.name'), nullable=False)
@@ -24,6 +27,7 @@ class Result(db.Model):
     test_id = db.Column(db.Integer, primary_key=True)
 
 
+# serialization schema for Person
 class PersonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Person
@@ -32,10 +36,12 @@ class PersonSchema(ma.SQLAlchemyAutoSchema):
         include_relationships = True
 
 
+# create distinct data serialization schema objects for a single person and multiple people
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)
 
 
+# serialization schema for Result
 class ResultSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Result
@@ -45,9 +51,12 @@ class ResultSchema(ma.SQLAlchemyAutoSchema):
         include_relationships = True
 
 
+# create distinct data serialization schema objects for a single result and multiple results
 result_schema = ResultSchema()
 results_schema = ResultSchema(many=True)
 
+# add custom SQL to execute after "person" and "result" table creation.
+# creates a trigger to execute upserts on the "person" table based inserts or updates on the "result" table.
 trigger_ddl = """
     CREATE OR REPLACE FUNCTION update_covid_status() RETURNS trigger AS $update_covid_status$
         BEGIN
@@ -65,14 +74,14 @@ trigger_ddl = """
     BEFORE INSERT OR UPDATE ON result
         FOR EACH ROW EXECUTE PROCEDURE update_covid_status();"""
 
-
+# call SQLAlchemy event lister function to issue custom SQL after tables are created based on model metadata
 event.listen(
     db.Model.metadata,
     "after_create",
     DDL(trigger_ddl)
 )
 
+# the SQLAlchemy create_all() method will issue queries that first check for the existence of each individual table
+# defined in the model metadata, and if not found will issue the CREATE statements
 with app.app_context():
     db.Model.metadata.create_all(db.engine)
-
-
